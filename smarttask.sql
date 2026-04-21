f7da1e50a69793fe652636aa1bab268dee3233c3
chifusenpai <124533893+chifusenpai@users.noreply.github.com>
2026-04-21 18:00:45 +0100

    db

---
+++ smarttask.sql
@@ -0,0 +1,340 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 21 avr. 2026 à 18:53
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `smarttask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `collaborator`
+--
+
+CREATE TABLE `collaborator` (
+  `post` varchar(255) NOT NULL,
+  `team` varchar(255) NOT NULL,
+  `enterprise_code` varchar(20) NOT NULL,
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20260304012952', '2026-03-04 01:31:18', 83);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `formation`
+--
+
+CREATE TABLE `formation` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `description` longtext DEFAULT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  `duree` int(11) DEFAULT NULL,
+  `niveau` varchar(50) NOT NULL,
+  `categorie` varchar(100) DEFAULT NULL,
+  `statut` varchar(50) NOT NULL,
+  `google_event_id` varchar(255) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `formation`
+--
+
+INSERT INTO `formation` (`id`, `titre`, `description`, `date_debut`, `date_fin`, `duree`, `niveau`, `categorie`, `statut`, `google_event_id`, `capacity`) VALUES
+(29, 'Best Practice', 'asdad', '2026-03-06', '2026-03-06', 15, 'debutant', 'Organization', 'active', 'dakp167ncmgbsgvhsvsha03eac', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscription`
+--
+
+CREATE TABLE `inscription` (
+  `id` int(11) NOT NULL,
+  `date_inscription` datetime NOT NULL,
+  `statut` varchar(50) NOT NULL,
+  `progression` int(11) NOT NULL,
+  `certificat` tinyint(4) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `formation_id` int(11) NOT NULL,
+  `absent_follow_up_sent_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `inscription`
+--
+
+INSERT INTO `inscription` (`id`, `date_inscription`, `statut`, `progression`, `certificat`, `user_id`, `formation_id`, `absent_follow_up_sent_at`) VALUES
+(25, '2026-03-05 12:10:39', 'en_cours', 0, 0, 1, 29, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `manager`
+--
+
+CREATE TABLE `manager` (
+  `level` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `enterprise_code` varchar(20) NOT NULL,
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `manager`
+--
+
+INSERT INTO `manager` (`level`, `department`, `enterprise_code`, `iduser`) VALUES
+('jenior', 'it', 'AGR7973', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projet`
+--
+
+CREATE TABLE `projet` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` longtext DEFAULT NULL,
+  `date_debut` date NOT NULL,
+  `date_echeance` date NOT NULL,
+  `statut` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tache`
+--
+
+CREATE TABLE `tache` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(255) NOT NULL,
+  `priorite` varchar(50) NOT NULL,
+  `date_limite` date NOT NULL,
+  `etat` varchar(50) NOT NULL,
+  `projet_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `iduser` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `google_id` varchar(100) DEFAULT NULL,
+  `linkedin_id` varchar(100) DEFAULT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`roles`)),
+  `is_enabled` tinyint(4) NOT NULL DEFAULT 1,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_token_expires_at` datetime DEFAULT NULL,
+  `avatar_name` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `face_embedding` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`face_embedding`)),
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`iduser`, `name`, `email`, `password`, `google_id`, `linkedin_id`, `roles`, `is_enabled`, `reset_token`, `reset_token_expires_at`, `avatar_name`, `updated_at`, `face_embedding`, `type`) VALUES
+(1, 'mouhanned', 'mouhannedkhemir2@gmail.com', '$2y$13$lmHPsSphECnbbyndJnBKv.VQPUDjq2Fjpu4HMUZbzI6eSKJkmTntq', NULL, NULL, '[]', 1, NULL, NULL, NULL, NULL, NULL, 'manager');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `collaborator`
+--
+ALTER TABLE `collaborator`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `formation`
+--
+ALTER TABLE `formation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `inscription`
+--
+ALTER TABLE `inscription`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_5E90F6D6A76ED395` (`user_id`),
+  ADD KEY `IDX_5E90F6D65200282E` (`formation_id`);
+
+--
+-- Index pour la table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`iduser`),
+  ADD UNIQUE KEY `UNIQ_FA2425B93CDF284` (`enterprise_code`);
+
+--
+-- Index pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750` (`queue_name`,`available_at`,`delivered_at`,`id`);
+
+--
+-- Index pour la table `projet`
+--
+ALTER TABLE `projet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_93872075C18272` (`projet_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`iduser`),
+  ADD UNIQUE KEY `UNIQ_8D93D64976F5C865` (`google_id`),
+  ADD UNIQUE KEY `UNIQ_8D93D64999ABDB52` (`linkedin_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `formation`
+--
+ALTER TABLE `formation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT pour la table `inscription`
+--
+ALTER TABLE `inscription`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `projet`
+--
+ALTER TABLE `projet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `tache`
+--
+ALTER TABLE `tache`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `collaborator`
+--
+ALTER TABLE `collaborator`
+  ADD CONSTRAINT `FK_606D487C5E5C27E9` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `inscription`
+--
+ALTER TABLE `inscription`
+  ADD CONSTRAINT `FK_5E90F6D65200282E` FOREIGN KEY (`formation_id`) REFERENCES `formation` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_5E90F6D6A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`iduser`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `manager`
+--
+ALTER TABLE `manager`
+  ADD CONSTRAINT `FK_FA2425B95E5C27E9` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD CONSTRAINT `FK_93872075C18272` FOREIGN KEY (`projet_id`) REFERENCES `projet` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
